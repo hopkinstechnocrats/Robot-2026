b3b5536c07b782687cff84762f229e639bc3ad50
Marilyn <136532458+Freezerat@users.noreply.github.com>
2026-01-31 15:27:26 -0600

    Initial Commit
    added limelight helpers and vision doesn't work currently.

---
+++ src/main/java/frc/robot/swerve/vision.java
@@ -0,0 +1,13 @@
+package frc.robot.swerve;
+
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.RawFiducial;
+
+public class vision {
+    private RawFiducial[] fiducials = LimelightHelpers.getRawFiducials("");
+    
+    for (RawFiducial fiducial : fiducials) {
+        int id = fiducial.id;                    // Tag ID
+        System.out.print(id);
+    }//*/
+}
