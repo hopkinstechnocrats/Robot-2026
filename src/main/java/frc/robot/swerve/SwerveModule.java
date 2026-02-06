194cc082a32e5f60f7a70a5ff80f3f19662953c4
asher <asher.fix@hopkinsrobotics.org>
2026-02-05 20:18:49 -0600

    added untested PID tunable numbers

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -15,9 +15,13 @@ import com.ctre.phoenix6.swerve.SwerveModuleConstantsFactory;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.TunableNumber;
 
 public class SwerveModule extends SubsystemBase{
 
@@ -32,12 +36,27 @@ public class SwerveModule extends SubsystemBase{
     
     AnalogEncoder m_absoluteEncoder;
 
+    NetworkTableInstance inst;
+    NetworkTable table;
+
+    TunableNumber kPInput;
+    TunableNumber kIInput;
+    TunableNumber kDInput;
+
     final PositionVoltage m_turnRequest = new PositionVoltage(0).withSlot(0);
     final VelocityVoltage m_driveRequest = new VelocityVoltage(0).withSlot(0);
 
     SwerveModuleState m_moduleState;
 
     SwerveModule(int driveID, int turnID, int absEncoderPort, double absEcoderOffset){
+        inst = NetworkTableInstance.getDefault();
+        table = inst.getTable("Tunable Numbers");
+
+        kPInput = new TunableNumber("/Tunable Numbers/kPInput", Constants.SwerveConstants.k_driveKP);
+        kIInput = new TunableNumber("/Tunable Numbers/kIInput", Constants.SwerveConstants.k_driveKI);
+        kDInput = new TunableNumber("/Tunable Numbers/kDInput", Constants.SwerveConstants.k_driveKD);
+
+
         m_driveMotor = new TalonFX(driveID, new CANBus("GertrudeGreyser"));
         m_turnMotor = new TalonFX(turnID, new CANBus("GertrudeGreyser"));
 
@@ -69,6 +88,21 @@ public class SwerveModule extends SubsystemBase{
         m_turnMotor.getConfigurator().apply(m_turnConfig);
 
         m_turnMotor.getConfigurator().setPosition(m_absoluteEncoder.get()-absEcoderOffset);
+    }
+
+     @Override
+    public void periodic(){
+        if(DriverStation.isTestEnabled() && kPInput.hasChanged(hashCode())){
+            m_driveConfig.kP = kPInput.getAsDouble();
+        }
+
+        if(DriverStation.isTestEnabled() && kIInput.hasChanged(hashCode())){
+            m_driveConfig.kI = kIInput.getAsDouble();
+        }
+
+        if(DriverStation.isTestEnabled() && kDInput.hasChanged(hashCode())){
+            m_driveConfig.kD = kDInput.getAsDouble();
+        }
     }
 
     public void Drive(SwerveModuleState moduleState){
