a1e32f65cab9beceed4b70e5dcaf7f3cc17dbae7
Lily <lily.lietzau@hopkinsrobotics.org>
2026-02-07 14:25:57 -0600

    driving and turning auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,17 +11,20 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.swerve.Gyro;
 import frc.robot.swerve.Swervedrive;
+import frc.robot.autos.Autos;
 import frc.robot.commands.DriveCommands;
 import frc.robot.commands.TeleopDrive;
 
 public class RobotContainer {
 
     Swervedrive m_swerve = new Swervedrive();
+    Autos auto = new Autos();
     CommandXboxController driveController = new CommandXboxController(Constants.ControlConstants.k_driverPort);
 
     private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
     public RobotContainer() {
+        m_chooser.setDefaultOption("forward auto", auto.complexAuto(m_swerve, 2)); //spped x & y is meters/second
         m_swerve.setDefaultCommand(
             new TeleopDrive(m_swerve, () -> driveController.getLeftY(), () -> driveController.getLeftX(), () -> driveController.getRightX()) 
         );
@@ -35,7 +38,6 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        System.out.println("Auto selected (getAutonomousCommand)");
         return m_chooser.getSelected();
     }
 }
