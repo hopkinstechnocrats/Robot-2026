64cb8ff9b86a201eae98088ee968650d3922c544
Lily <lily.lietzau@hopkinsrobotics.org>
2026-02-28 11:22:53 -0600

    untested update gyro button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -34,7 +37,7 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        
+         driveController.a().onTrue(Commands.run(() -> m_swerve.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));
     }
 
     public Command getAutonomousCommand() {
--- src/main/java/frc/robot/swerve/Swervedrive.java
+++ src/main/java/frc/robot/swerve/Swervedrive.java
@@ -132,4 +132,21 @@ public class Swervedrive extends SubsystemBase{
     public Rotation2d getRotation(){
         return gyro.getRotation();
     }
+
+    public void resetOdometry(Pose2d pose) {
+        m_poseEstimator.resetPosition(gyro.getRotation(), this.getModulePositions(), pose);
+        ChassisSpeeds robotRelativeSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(new ChassisSpeeds(0, 0, 0), gyro.getRotation());
+        m_swerveKinematics.toSwerveModuleStates(robotRelativeSpeeds);
+    }
+
+    public SwerveModulePosition[] getModulePositions(){
+        SwerveModulePosition[] positions = new SwerveModulePosition[]{
+            fL.getModulePosition(),
+            fR.getModulePosition(),
+            bL.getModulePosition(),
+            bR.getModulePosition()
+        };
+        
+        return positions;
+    }
 }
