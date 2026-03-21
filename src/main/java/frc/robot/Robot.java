342490d27df60a54ab50b791a0643e927869b9f0
Lucian <lucian.pier@hopkinsrobotics.org>
2026-03-21 13:44:04 -0500

    added camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import edu.wpi.first.cameraserver.CameraServer;
+
 public class Robot extends TimedRobot {
+  public Robot() {
+    CameraServer.startAutomaticCapture();
+  }
+
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
