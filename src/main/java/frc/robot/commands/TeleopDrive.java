29d8b7ed0326295590cbede8b086efb683e9e758
AJAmelse <artemis.amelse@hopkinsrobotics.org>
2026-03-24 19:58:38 -0500

    don't know why not work

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -5,6 +5,9 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -27,6 +30,10 @@ public class TeleopDrive extends Command{
     
     private Rotation2d desiredRotation;
 
+    NetworkTableInstance inst;
+    NetworkTable table;
+    DoubleEntry calculatedOmega;
+
     private double m_xOut;
     private double m_yOut;
     private double m_omegaOut;
@@ -41,7 +48,11 @@ public class TeleopDrive extends Command{
         m_slowMode = slowMode;
         m_fastMode = fastMode;
         m_bumpAngle = bumpAngle;
-        desiredRotation = new Rotation2d(Math.PI);
+        desiredRotation = new Rotation2d(Math.PI/4);
+
+        inst = NetworkTableInstance.getDefault();
+        table = inst.getTable("Drive Command");
+        calculatedOmega = table.getDoubleTopic("Calculated Omega").getEntry(0);
 
         addRequirements(swervedrive);
     }
@@ -51,13 +62,20 @@ public class TeleopDrive extends Command{
 
     @Override
     public void execute(){
+        if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Red){
+            invert = -1;
+        }
+        if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Blue){
+            invert = 1;
+        }
+
         if(m_bumpAngle.getAsBoolean()){
 
 
             // Create PID controller
             ProfiledPIDController angleController = 
                 new ProfiledPIDController(
-                    5.0, 0.0, 0.4, 
+                    5.0, 0.0, 0.0, 
                     new TrapezoidProfile.Constraints(8, 20)); 
             angleController.enableContinuousInput(-Math.PI, Math.PI);
 
@@ -65,6 +83,8 @@ public class TeleopDrive extends Command{
             double omega =
                 angleController.calculate(
                     m_swerve.getRotation().getRadians(), desiredRotation.getRadians());
+
+            calculatedOmega.set(omega);
 
             // Convert to field relative speeds & send command
 
@@ -97,39 +117,12 @@ public class TeleopDrive extends Command{
             
             m_omegaOut *= Constants.SwerveConstants.k_maxAngularSpeedRadPerSec;
 
-            ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(m_xOut, m_yOut, m_omegaOut, m_swerve.getRotation());
+            ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(m_xOut * invert, m_yOut * invert, m_omegaOut, m_swerve.getRotation());
+
             //ChassisSpeeds speeds = new ChassisSpeeds(m_xOut, m_yOut, m_omegaOut);
 
             m_swerve.Drive(speeds);
         }
-        if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Red){
-            invert = -1;
-        }
-        if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == Alliance.Blue){
-            invert = 1;
-        }
-        m_xOut = MathUtil.applyDeadband(-m_x.getAsDouble(), Constants.ControlConstants.k_driveControllerDeadband);
-        m_yOut = MathUtil.applyDeadband(-m_y.getAsDouble(), Constants.ControlConstants.k_driveControllerDeadband);
-        m_omegaOut = MathUtil.applyDeadband(Constants.SwerveConstants.k_blaireMode*m_omega.getAsDouble(), Constants.ControlConstants.k_driveControllerDeadband);
-
-        if(m_fastMode.getAsDouble() > 0.5){
-            m_xOut *= Constants.SwerveConstants.k_slowMaxLinearSpeenMetersPerSecond;
-            m_yOut *= Constants.SwerveConstants.k_slowMaxLinearSpeenMetersPerSecond;
-        }else if(m_bumpMode.getAsDouble() > 0.5){
-            m_xOut *= Constants.SwerveConstants.k_maxLinearSpeedMeterPerSecond;
-            m_yOut *= Constants.SwerveConstants.k_maxLinearSpeedMeterPerSecond;
-
-        }else{
-            m_xOut *= Constants.SwerveConstants.k_bumpMaxLinearSpeenMetersPerSecond;
-            m_yOut *= Constants.SwerveConstants.k_bumpMaxLinearSpeenMetersPerSecond;
-        }
-        
-        
-        m_omegaOut *= Constants.SwerveConstants.k_maxAngularSpeedRadPerSec;
-
-        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(m_xOut * invert, m_yOut * invert, m_omegaOut, m_swerve.getRotation());
-        //ChassisSpeeds speeds = new ChassisSpeeds(m_xOut, m_yOut, m_omegaOut);
-
     }
 
     @Override
