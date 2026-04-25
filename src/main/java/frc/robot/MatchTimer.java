0bcbf19d90d67f5946ccbb5fd4cfab261e97a98b
asher <asher.fix@hopkinsrobotics.org>
2026-04-25 12:47:07 -0500

    woking everything as far as I know

--- src/main/java/frc/robot/MatchTimer.java
+++ src/main/java/frc/robot/MatchTimer.java
@@ -124,7 +124,8 @@ public class MatchTimer {
     
     public void update(){        
             
-        matchTime = (int)DriverStation.getMatchTime();
+        matchTime = (int)DriverStation.getMatchTime();//pulls live match
+
         if (isHubActive() == true){
             hubIsEnabled.set("Active!");
             hubColors.set(true);
@@ -134,13 +135,13 @@ public class MatchTimer {
         } else {
             hubIsEnabled.set("Auto");
             hubColors.set(true);
-        }
+        }//sets whether or not the hub is active
 
         if (DriverStation.isAutonomousEnabled()){
             shift.set("Auto");
             gameTime.set(matchTime + 140);
             timeDifference = 0;
-        }
+        }//sets auto time, resets the time left in shift
 
         if(powerRumble == true){
             if (timeLeftInShift.get() % 2 == 1 && timeLeftInShift.get() < 6  && !DriverStation.isAutonomousEnabled()){
@@ -151,83 +152,82 @@ public class MatchTimer {
             if(timeLeftInShift.get() % 2 == 0 && timeLeftInShift.get() < 5 && !DriverStation.isAutonomousEnabled()){
             driveController.setRumble(GenericHID.RumbleType.kLeftRumble, .3);
             operatorController.setRumble(GenericHID.RumbleType.kLeftRumble, .3);
-            }
+            //Pulsing rumble when active
+
         }else{
-        
             if (timeLeftInShift.get() == 5 && !DriverStation.isAutonomousEnabled()){
                 driveController.setRumble(GenericHID.RumbleType.kLeftRumble, .3);
                 operatorController.setRumble(GenericHID.RumbleType.kLeftRumble, .3);
             }
-        }
-    
+        }//constant low rumble when inactive
         
         if (timeLeftInShift.get() == 0 && !DriverStation.isAutonomousEnabled()){
             driveController.setRumble(GenericHID.RumbleType.kBothRumble, 0);
             operatorController.setRumble(GenericHID.RumbleType.kBothRumble, 0);
-        }
+        }//rumble stop
 
         if(!DriverStation.isAutonomousEnabled()){
             gameTime.set(matchTime);
-        }
+        }//updating match time
 
-        timeLeftInShift.set(matchTime - timeDifference);//to get the difference
+        timeLeftInShift.set(matchTime - timeDifference);//to get the difference for time left in the shift
 
         if (matchTime == 139 && !DriverStation.isAutonomousEnabled()){
             shift.set("Transition Shift");
-            timeDifference = 140 - 10;
-            if (firstShift== true){
+            timeDifference = 140 - 10;// current match time - lenght of shift
+            if (firstShift == true){
                     powerRumble = true;
                 }else{
                     powerRumble = false;
                 }
-            //start transition shift
-            } else if (matchTime == 130 && !DriverStation.isAutonomousEnabled()) {
-                shift.set("Shift 1");
-                timeDifference = 130-25;
-                if (isHubActive() == false){
-                    powerRumble = true;
-                }else{
-                    powerRumble = false;
-                }
-                //start shift 1
-            } else if (matchTime == 105 && !DriverStation.isAutonomousEnabled()) {
-                shift.set("Shift 2");
-                timeDifference = 105-25;
-                if (isHubActive() == false){
-                    powerRumble = true;
-                }else{
-                    powerRumble = false;
-                }
-                //start shift 2
-            } else if (matchTime == 80 && !DriverStation.isAutonomousEnabled()) {
-                shift.set("Shift 3");
-                timeDifference = 80-25;
-                if (isHubActive() == false){
-                    powerRumble = true;
-                }else{
-                    powerRumble = false;
-                }
-                //start shift 3
-            } else if (matchTime == 55 && !DriverStation.isAutonomousEnabled()) {
-                shift.set("Shift 4");
-                timeDifference = 55-25;
-                if (isHubActive() == false){
-                    powerRumble = true;
-                }else{
-                    powerRumble = false;
-                }
-                //start shift 4
-            } else if (matchTime == 30 && !DriverStation.isAutonomousEnabled()) {
-                shift.set("Endgame");
-                timeDifference = 30-30;
+            //start transition shift, set correct rumble type, sets the time left in shift
+        } else if (matchTime == 130 && !DriverStation.isAutonomousEnabled()) {
+            shift.set("Shift 1");
+            timeDifference = 130-25;
+            if (isHubActive() == false){
                 powerRumble = true;
-                //start endgame
-            } else if (matchTime == 0 && !DriverStation.isAutonomousEnabled()) {
-                timeDifference = 0;
-                shift.set("Game Over");
-                //End          
+            }else{
+                powerRumble = false;
+            }
+            //start shift 1, set correct rumble type, sets the time left in shift
+        } else if (matchTime == 105 && !DriverStation.isAutonomousEnabled()) {
+            shift.set("Shift 2");
+            timeDifference = 105-25;
+            if (isHubActive() == false){
+                powerRumble = true;
+            }else{
+                powerRumble = false;
+            }
+                //start shift 2, set correct rumble type, sets the time left in shift
+        } else if (matchTime == 80 && !DriverStation.isAutonomousEnabled()) {
+            shift.set("Shift 3");
+            timeDifference = 80-25;
+            if (isHubActive() == false){
+                powerRumble = true;
+            }else{
+                powerRumble = false;
+            }
+            //start shift 3, set correct rumble type, sets the time left in shift
+        } else if (matchTime == 55 && !DriverStation.isAutonomousEnabled()) {
+            shift.set("Shift 4");
+            timeDifference = 55-25;
+            if (isHubActive() == false){
+                powerRumble = true;
+            }else{
+                powerRumble = false;
+            }
+            //start shift 4, set correct rumble type, sets the time left in shift
+        } else if (matchTime == 30 && !DriverStation.isAutonomousEnabled()) {
+            shift.set("Endgame");
+            timeDifference = 30-30;
+            powerRumble = true;
+            //start endgame, set correct rumble type, sets the time left in shift
+        } else if (matchTime == 0 && !DriverStation.isAutonomousEnabled()) {
+            timeDifference = 0;
+            shift.set("Game Over");
+             //End of game      
             } else {       
             }
+        }
     }
-
 }
